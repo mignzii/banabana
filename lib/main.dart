fa8da9e57c445190af9ad87d2ec9fcb751571d6b
mignzii <mignzii99@gmail.com>
2026-05-10 22:07:43 +0000

    chore: enforce linter rules and fix StatelessWidget in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -5,14 +5,15 @@ void main() {
   runApp(const ProviderScope(child: BanaBanaApp()));
 }
 
-class BanaBanaApp extends ConsumerWidget {
+class BanaBanaApp extends StatelessWidget {
   const BanaBanaApp({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  Widget build(BuildContext context) {
     return MaterialApp(
       title: 'BanaBana Business',
       theme: ThemeData(useMaterial3: true),
+      // TODO: replace with MaterialApp.router once appRouterProvider is ready (Task 9)
       home: const Scaffold(body: Center(child: Text('BanaBana B2B'))),
       debugShowCheckedModeBanner: false,
     );
